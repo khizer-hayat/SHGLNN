d569ee3dd4ceb253ee8b6a0bea5c718f359205a3
Malik Khizar Hayat <khizerhayat92@gmail.com>
2023-10-12 05:19:27 +1100

    init

--- layers.py
+++ layers.py
@@ -2,23 +2,36 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-class NodeConv(nn.Module):
-    def __init__(self, in_dim, out_dim):
-        super(NodeConv, self).__init__()
-        self.W_v = nn.Linear(in_dim, out_dim)
-        self.W_e = nn.Linear(in_dim, out_dim)
+class NodeConvolution(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(NodeConvolution, self).__init__()
+        self.W_v = nn.Linear(in_channels, out_channels)
+        self.W_e = nn.Linear(in_channels, out_channels)
+        self.P = nn.Parameter(torch.FloatTensor(out_channels, out_channels))
         self.activation = nn.ReLU()
 
-    def forward(self, x, H):
-        out = self.activation(self.W_v(H @ x) + self.W_e(H.T @ x))
+    def forward(self, x, H, D_v_inv, D_e_inv):
+        out_1 = self.W_v(x)
+        out_2 = self.W_e(x)
+        out = self.activation(self.P @ (D_v_inv @ H @ out_1 @ D_e_inv @ H.T @ x + D_v_inv @ H @ out_2 @ D_e_inv @ H.T @ x))
         return out
 
-class HyperedgeConv(nn.Module):
-    def __init__(self, in_dim, out_dim):
-        super(HyperedgeConv, self).__init__()
-        self.W = nn.Linear(in_dim, out_dim)
+class HyperedgeConvolution(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(HyperedgeConvolution, self).__init__()
+        self.W_q = nn.Linear(in_channels, out_channels)
+        self.W_o = nn.Linear(in_channels, out_channels)
         self.activation = nn.ReLU()
 
-    def forward(self, x, H, hyperedge_weights):
-        out = self.activation(self.W(H @ x))
-        return out * hyperedge_weights
+    def forward(self, x, M, K, alpha):
+        out = self.activation((1/(M.size(0)+1)) * (torch.sum(self.W_q(x) * alpha, dim=0) + self.W_o(x)))
+        return out
+
+class Attention(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(Attention, self).__init__()
+        self.attention_vector = nn.Linear(in_channels, out_channels)
+
+    def forward(self, v, K):
+        attention_score = F.softmax(self.attention_vector(v).mm(K.T), dim=-1)
+        return attention_score
