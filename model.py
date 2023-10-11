f0a1609309f1152f1e0fb5fa7a51fb9e3219331f
Malik Khizar Hayat <khizerhayat92@gmail.com>
2023-10-12 05:37:43 +1100

    init

--- model.py
+++ model.py
@@ -1,18 +1,17 @@
-import torch
 import torch.nn as nn
-import torch.nn.functional as F
-from layers import NodeConv, HyperedgeConv
+from layers import NodeConvolution, HyperedgeConvolution, Attention, ContextAwarePooling
 
 class SHGLNN(nn.Module):
-    def __init__(self, in_dim, hidden_dim, out_dim):
+    def __init__(self, in_dim, hidden_dim):
         super(SHGLNN, self).__init__()
-        self.node_conv = NodeConv(in_dim, hidden_dim)
-        self.hyperedge_conv = HyperedgeConv(hidden_dim, out_dim)
-        self.dropout = nn.Dropout(0.5)
-        self.fc = nn.Linear(out_dim, out_dim)
+        self.node_conv = NodeConvolution(in_dim, hidden_dim)
+        self.hyperedge_conv = HyperedgeConvolution(hidden_dim, hidden_dim)
+        self.attention = Attention(hidden_dim, hidden_dim)
+        self.context_pooling = ContextAwarePooling(hidden_dim)
 
-    def forward(self, x, H, hyperedge_weights):
-        x = self.node_conv(x, H)
-        x = self.hyperedge_conv(x, H, hyperedge_weights)
-        x = self.dropout(x)
-        return self.fc(x)
+    def forward(self, x, H, K, M, D_v_inv, D_e_inv, E_intra, E_inter):
+        x = self.node_conv(x, H, D_v_inv, D_e_inv)
+        alpha = self.attention(x, K)
+        x = self.hyperedge_conv(x, M, K, alpha)
+        graph_embedding = self.context_pooling(x, E_intra, E_inter)
+        return graph_embedding
