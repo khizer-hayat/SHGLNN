19d31882af105326f131dace9b4c4402da373990
Malik Khizar Hayat <khizerhayat92@gmail.com>
2025-04-17 04:40:48 +1000

    dataset_loader.py

--- dataset_loader.py
+++ dataset_loader.py
@@ -15,3 +15,11 @@ class CustomDatasetLoader:
         dataset = self.load_dataset()
         loader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle)
         return loader
+    def total_nodes_in_a_graph(self):
+        """Calculate the total number of nodes in the dataset."""
+        dataset = self.load_dataset()
+        total_nodes = 0
+        # Loop through each graph in the dataset and sum the number of nodes
+        for data in dataset:
+            total_nodes += data.num_nodes  # `num_nodes` is the number of nodes in each graph
+        return total_nodes, dataset
